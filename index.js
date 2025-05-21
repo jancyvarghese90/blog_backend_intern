30d57ee9b05a61df04d6792415dc18917e40b281
jancyvarghese90 <jancyvarghese90@gmail.com>
2025-05-21 12:56:00 +0300

    frontend link updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function startServer() {
     typeDefs,
     resolvers,
     cors: {
-      origin: '*', // your frontend
+      origin: 'http://localhost:3000', // your frontend
       credentials: true,
     },
   });
